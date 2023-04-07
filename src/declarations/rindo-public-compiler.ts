9619b0f927003ffbb46f081bcb06c92dcecda626
NKDuy <kn145660@gmail.com>
2023-04-07 19:21:07 +0700

    feat(config): add `enableImportInjection` flag

--- src/declarations/rindo-public-compiler.ts
+++ src/declarations/rindo-public-compiler.ts
@@ -302,8 +302,18 @@ export interface ConfigExtras {
    * loading components when using a bundler such as Wite or Parcel. Setting this flag to `true` will change how Rindo
    * lazily loads components in a way that works with additional bundlers. Setting this flag to `true` will increase
    * the size of the compiled output. Defaults to `false`.
+   * @deprecated This flag has been deprecated in favor of `enableImportInjection`, which provides the same
+   * functionality. `experimentalImportInjection` will be removed in a future major version of Rindo.
    */
   experimentalImportInjection?: boolean;
+
+  /**
+   * Projects that use a Rindo library built using the `dist` output target may have trouble lazily
+   * loading components when using a bundler such as Vite or Parcel. Setting this flag to `true` will change how Rindo
+   * lazily loads components in a way that works with additional bundlers. Setting this flag to `true` will increase
+   * the size of the compiled output. Defaults to `false`.
+   */
+  enableImportInjection?: boolean;
 
   /**
    * Dispatches component lifecycle events. Mainly used for testing. Defaults to `false`.
