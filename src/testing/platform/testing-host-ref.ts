ffbe799b4abc32ce96797ac8ddfa35e0ee40c958
NKDuy <nkduy.dev@gmail.com>
2023-06-01 18:48:02 +0700

    chore(test): add jsdoc to testing

--- src/testing/platform/testing-host-ref.ts
+++ src/testing/platform/testing-host-ref.ts
@@ -3,11 +3,27 @@ import { addHostEventListeners } from '@runtime';
 
 import { hostRefs } from './testing-constants';
 
-export const getHostRef = (elm: d.RuntimeRef | undefined) => {
+/**
+ * Retrieve the data structure tracking the component by its runtime reference
+ * @param elm the reference to the element
+ * @returns the corresponding Rindo reference data structure, or undefined if one cannot be found
+ */
+export const getHostRef = (elm: d.RuntimeRef | undefined): d.HostRef | undefined => {
   return hostRefs.get(elm);
 };
 
-export const registerInstance = (lazyInstance: any, hostRef: d.HostRef) => {
+/**
+ * Add the provided `hostRef` instance to the global {@link hostRefs} map, using the provided `lazyInstance` as a key.
+ * @param lazyInstance a Rindo component instance
+ * @param hostRef an optional reference to Rindo's tracking data for the component. If none is provided, one will be created.
+ * @returns the updated `hostRefs` data structure
+ * @throws if the provided `lazyInstance` coerces to `null`, or if the `lazyInstance` does not have a `constructor`
+ * property
+ */
+export const registerInstance = (
+  lazyInstance: any,
+  hostRef: d.HostRef | null | undefined
+): Map<d.RuntimeRef, d.HostRef> => {
   if (lazyInstance == null || lazyInstance.constructor == null) {
     throw new Error(`Invalid component constructor`);
   }
@@ -24,7 +40,12 @@ export const registerInstance = (lazyInstance: any, hostRef: d.HostRef) => {
   return hostRefs.set(lazyInstance, hostRef);
 };
 
-export const registerHost = (elm: d.HostElement, cmpMeta: d.ComponentRuntimeMeta) => {
+/**
+ * Create a new {@link d.HostRef} instance to the global {@link hostRefs} map, using the provided `elm` as a key.
+ * @param elm an HTMLElement instance associated with the Rindo component
+ * @param cmpMeta the component compiler metadata associated with the component
+ */
+export const registerHost = (elm: d.HostElement, cmpMeta: d.ComponentRuntimeMeta): void => {
   const hostRef: d.HostRef = {
     $flags$: 0,
     $hostElement$: elm,
