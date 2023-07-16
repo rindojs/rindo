2cea574114dfa1b5c7575a075fa945d19512cd93
NKDuy <nkduy.dev@gmail.com>
2023-07-16 19:10:46 +0700

    chore(props): removal of deprecated connect and context APIs

--- src/compiler/transformers/core-runtime-apis.ts
+++ src/compiler/transformers/core-runtime-apis.ts
@@ -3,8 +3,6 @@ import type * as d from '../../declarations';
 export const ATTACH_SHADOW = '__rindo_attachShadow';
 export const CREATE_EVENT = '__rindo_createEvent';
 export const DEFINE_CUSTOM_ELEMENT = '__rindo_defineCustomElement';
-export const GET_CONNECT = '__rindo_getConnect';
-export const GET_CONTEXT = '__rindo_getContext';
 export const GET_ELEMENT = '__rindo_getElement';
 export const HOST = '__rindo_Host';
 export const HTML_ELEMENT = 'HTMLElement';
@@ -17,8 +15,6 @@ export const RUNTIME_APIS = {
   attachShadow: `attachShadow as ${ATTACH_SHADOW}`,
   createEvent: `createEvent as ${CREATE_EVENT}`,
   defineCustomElement: `defineCustomElement as ${DEFINE_CUSTOM_ELEMENT}`,
-  getConnect: `getConnect as ${GET_CONNECT}`,
-  getContext: `getContext as ${GET_CONTEXT}`,
   getElement: `getElement as ${GET_ELEMENT}`,
   h: `h as ${H}`,
   legacyH: `h`,
