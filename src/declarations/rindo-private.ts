b62e54a13d97e6ec231f7d61988ec0df95952862
NKDuy <nkduy.dev@gmail.com>
2023-12-01 07:47:09 +0700

    refactor(telemetry): remove browser-specific code from telemetry

--- src/declarations/rindo-private.ts
+++ src/declarations/rindo-private.ts
@@ -2635,7 +2635,6 @@ export interface TrackableData {
   cpu_model: string | undefined;
   duration_ms: number | undefined;
   has_app_pwa_config: boolean;
-  is_browser_env: boolean;
   os_name: string | undefined;
   os_version: string | undefined;
   packages: string[];
