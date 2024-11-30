8df9963dab985d65d94b5b50bc3b5953320d6882
NKDuy <nkduy.dev@gmail.com>
2024-11-30 09:55:27 +0000

    chore(build): remove `rollup` build infrastructure

--- scripts/esbuild/internal.ts
+++ scripts/esbuild/internal.ts
@@ -1,15 +1,16 @@
+import { generateDtsBundle } from 'dts-bundle-generator';
 import type { BuildOptions as ESBuildOptions } from 'esbuild';
 import fs from 'fs-extra';
 import { join } from 'path';
 
-import { copyRindoInternalDts } from '../bundles/internal';
+import { bundleDts, cleanDts } from '../utils/bundle-dts';
 import type { BuildOptions } from '../utils/options';
 import { writePkgJson } from '../utils/write-pkg-json';
 import { getInternalAppDataBundles } from './internal-app-data';
 import { getInternalClientBundles } from './internal-platform-client';
 import { getInternalPlatformHydrateBundles } from './internal-platform-hydrate';
 import { getInternalTestingBundle } from './internal-platform-testing';
-import { getBaseEsbuildOptions, runBuilds } from './util';
+import { getBaseEsbuildOptions, runBuilds } from './utils';
 
 /**
  * Run the build for the `internal/` directory, copying and modifying files
@@ -98,3 +99,85 @@ const copyUtilsDtsFiles = async (opts: BuildOptions) => {
   // elsewhere like `import { result } from '@utils'` will resolve correctly
   await fs.writeFile(utilsIndexDtsPath, `export * as result from "./result"`);
 };
+
+async function copyRindoInternalDts(opts: BuildOptions, outputInternalDir: string) {
+  const declarationsInputDir = join(opts.buildDir, 'declarations');
+
+  // copy to @rindo/core/internal
+
+  // @rindo/core/internal/index.d.ts
+  const indexDtsSrcPath = join(declarationsInputDir, 'index.d.ts');
+  const indexDtsDestPath = join(outputInternalDir, 'index.d.ts');
+  let indexDts = cleanDts(await fs.readFile(indexDtsSrcPath, 'utf8'));
+  indexDts = prependExtModules(indexDts);
+  await fs.writeFile(indexDtsDestPath, indexDts);
+
+  // @rindo/core/internal/rindo-private.d.ts
+  const privateDtsSrcPath = join(declarationsInputDir, 'rindo-private.d.ts');
+  const privateDtsDestPath = join(outputInternalDir, 'rindo-private.d.ts');
+  let privateDts = cleanDts(await fs.readFile(privateDtsSrcPath, 'utf8'));
+
+  // @rindo/core/internal/child_process.d.ts
+  const childProcessSrcPath = join(declarationsInputDir, 'child_process.d.ts');
+  const childProcessDestPath = join(outputInternalDir, 'child_process.d.ts');
+
+  // we generate a tiny tiny bundle here of just
+  // `src/declarations/child_process.ts` so that `internal/rindo-private.d.ts`
+  // can import from `'./child_process'` without worrying about resolving the
+  // types from `node_modules`.
+  const childProcessDts = generateDtsBundle([
+    {
+      filePath: childProcessSrcPath,
+      libraries: {
+        // we need to mark this library so that types imported from it are inlined
+        inlinedLibraries: ['child_process'],
+      },
+      output: {
+        noBanner: true,
+        exportReferencedTypes: false,
+      },
+    },
+  ]).join('\n');
+  await fs.writeFile(childProcessDestPath, childProcessDts);
+
+  // the private `.d.ts` imports the `Result` type from the `@utils` module, so
+  // we need to rewrite the path so it imports from the right relative path
+  privateDts = privateDts.replace('@utils', './utils');
+  await fs.writeFile(privateDtsDestPath, privateDts);
+
+  // @rindo/core/internal/rindo-public.compiler.d.ts
+  const compilerDtsSrcPath = join(declarationsInputDir, 'rindo-public-compiler.d.ts');
+  const compilerDtsDestPath = join(outputInternalDir, 'rindo-public-compiler.d.ts');
+  const compilerDts = cleanDts(await fs.readFile(compilerDtsSrcPath, 'utf8'));
+  await fs.writeFile(compilerDtsDestPath, compilerDts);
+
+  // @rindo/core/internal/rindo-public-docs.d.ts
+  const docsDtsSrcPath = join(declarationsInputDir, 'rindo-public-docs.d.ts');
+  const docsDtsDestPath = join(outputInternalDir, 'rindo-public-docs.d.ts');
+  // We bundle with `dts-bundle-generator` here to ensure that when the `docs-json`
+  // OT writes a `docs.d.ts` file based on this file it is fully portable.
+  const docsDts = await bundleDts(opts, docsDtsSrcPath, {
+    // we want to suppress the `dts-bundle-generator` banner here because we do
+    // our own later on
+    noBanner: true,
+    // we also don't want the types which are inlined into our bundled file to
+    // be re-exported, which will change the 'surface' of the module
+    exportReferencedTypes: false,
+  });
+  await fs.writeFile(docsDtsDestPath, docsDts);
+
+  // @rindo/core/internal/rindo-public-runtime.d.ts
+  const runtimeDtsSrcPath = join(declarationsInputDir, 'rindo-public-runtime.d.ts');
+  const runtimeDtsDestPath = join(outputInternalDir, 'rindo-public-runtime.d.ts');
+  const runtimeDts = cleanDts(await fs.readFile(runtimeDtsSrcPath, 'utf8'));
+  await fs.writeFile(runtimeDtsDestPath, runtimeDts);
+
+  // @rindo/core/internal/rindo-ext-modules.d.ts (.svg/.css)
+  const srcExtModuleOutput = join(opts.srcDir, 'declarations', 'rindo-ext-modules.d.ts');
+  const dstExtModuleOutput = join(outputInternalDir, 'rindo-ext-modules.d.ts');
+  await fs.copyFile(srcExtModuleOutput, dstExtModuleOutput);
+}
+
+function prependExtModules(content: string) {
+  return `/// <reference path="./rindo-ext-modules.d.ts" />\n` + content;
+}
--- src/cli/load-compiler.ts
+++ src/cli/load-compiler.ts
@@ -1,15 +1,7 @@
 import type { CompilerSystem } from '../declarations';
 
 export const loadCoreCompiler = async (sys: CompilerSystem): Promise<CoreCompiler> => {
-  const compilerMod = await sys.dynamicImport!(sys.getCompilerExecutingPath());
-
-  // TODO(RINDO-1018): Remove Rollup Infrastructure
-  if ((globalThis as any).rindo) {
-    return (globalThis as any).rindo;
-  } else {
-    (globalThis as any).rindo = compilerMod;
-    return compilerMod;
-  }
+  return await sys.dynamicImport!(sys.getCompilerExecutingPath());
 };
 
 export type CoreCompiler = typeof import('@rindo/core/compiler');
