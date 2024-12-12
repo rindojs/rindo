aa065ce06b06fbd3b7ed2899dc21a09dac856c93
NKDuy <nkduy.dev@gmail.com>
2024-12-12 11:52:03 +0000

    feat(ci): remove need to compile scripts

--- scripts/index.ts
+++ scripts/index.ts
@@ -3,6 +3,6 @@ import { join } from 'path';
 import * as build from './build';
 
 // This path is relative to the final location of the compiled script, not its TypeScript source
-const rindoProjectRoot = join(__dirname, '..', '..');
+const rindoProjectRoot = join(__dirname, '..');
 const args = process.argv.slice(2);
 build.run(rindoProjectRoot, args);
