e2ddb42b7cfc46d1cc42f93ae48432b8db10b64b
NKDuy <nkduy.dev@gmail.com>
2025-01-12 16:43:13 +0000

    fix(testing): add testing sub module to export map

--- test/end-to-end/exportMap/index.js
+++ test/end-to-end/exportMap/index.js
@@ -6,6 +6,7 @@ const { h } = require('@rindo/core');
 const { MockDocument } = require('@rindo/core/mock-doc');
 const appData = require('@rindo/core/internal/app-data');
 const { createNodeLogger } = require('@rindo/core/sys/node');
+const { createTesting } = require('@rindo/core/testing');
 
 assert(typeof version === 'string');
 assert(typeof run, 'function');
@@ -13,3 +14,7 @@ assert(typeof h === 'function');
 assert(typeof MockDocument === 'function');
 assert(Object.keys(appData).length === 3);
 assert(typeof createNodeLogger === 'function');
+assert(typeof createTesting === 'function');
+
+console.log(`🎉 All CJS imports successfully resolved!`);
+console.log('✅ passed!\n');
--- test/end-to-end/exportMap/index.mts
+++ test/end-to-end/exportMap/index.mts
@@ -6,6 +6,7 @@ import { MockDocument } from '@rindo/core/mock-doc';
 import type { BuildConditionals } from '@rindo/core/internal';
 import { BUILD } from '@rindo/core/internal/app-data';
 import { createNodeLogger } from '@rindo/core/sys/node';
+import { createTesting } from '@rindo/core/testing';
 
 assert(typeof version === 'string');
 version.slice();
@@ -17,3 +18,7 @@ run.call;
 assert(typeof MockDocument === 'function');
 assert(typeof BUILD !== 'undefined');
 assert(typeof createNodeLogger === 'function');
+assert(typeof createTesting === 'function');
+
+console.log(`🎉 All ESM imports successfully resolved!`);
+console.log('✅ passed!\n');
